f4d09aaaa210cde5bcfb1e68f98738ecd979194d
AleFalcone27 <83794890+AleFalcone27@users.noreply.github.com>
2024-11-28 23:35:30 -0300

    Avance 2do sprint parte 2

---
+++ APP/src/app/components/especialista/perfil-especialista/perfil-especialista.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-perfil-especialista',
+  standalone: true,
+  imports: [],
+  templateUrl: './perfil-especialista.component.html',
+  styleUrl: './perfil-especialista.component.css'
+})
+export class PerfilEspecialistaComponent {
+
+}
